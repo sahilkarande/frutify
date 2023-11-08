b140da27a553445a48fe16683f97b7c40bb45d67
mohitXpain <97824051+mohitXpain@users.noreply.github.com>
2023-11-08 23:23:40 +0530

    popup

--- OriginalWEb/assets/js/main.js
+++ OriginalWEb/assets/js/main.js
@@ -32,3 +32,26 @@ document.querySelector('#login-btn').onclick = () => {
   cart.classList.remove('active');
   loginForm.classList.toggle('active');
 }
+
+let previewContainer = document.querySelector('.product-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.box-container .box').forEach(box =>{
+  box.onclick = () =>{
+    previewContainer.style.display = 'flex';
+    let name = box.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+       preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    previewContainer.style.display = 'none';
+  };
+});
